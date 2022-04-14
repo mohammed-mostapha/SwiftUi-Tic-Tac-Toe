a5181005410a5bb14d3b03cf4757fcc321b3eef8
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2022-04-14 23:17:24 +0200

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Mohamed Mostapha on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
